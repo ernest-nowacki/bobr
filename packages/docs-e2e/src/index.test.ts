98231e443f949809082c9f3ec11176e991d8a05d
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-11-20 11:35:59 +0100

    Add e2e tests

--- packages/docs-e2e/src/index.test.ts
+++ packages/docs-e2e/src/index.test.ts
@@ -1,1 +1,15 @@
-// nothing yet either
+import { test, expect } from '@playwright/test';
+
+test('docs page has correct bottom icons text', async ({ page }) => {
+  await page.goto('/');
+
+  const expectedTexts = [
+    'Iterate faster',
+    'Save time',
+    'Balanced runs'
+  ];
+
+  for (const text of expectedTexts) {
+    await expect(page.getByText(text)).toBeVisible();
+  }
+});
--- packages/web-e2e/src/index.test.ts
+++ packages/web-e2e/src/index.test.ts
@@ -1,1 +1,11 @@
-// nothing yet
+import { test, expect } from "@playwright/test";
+
+test("web page has correct bottom icons text", async ({ page }) => {
+  await page.goto("/");
+
+  const expectedTexts = ["Proper Cache", "Tree shaking", "No useless runs"];
+
+  for (const text of expectedTexts) {
+    await expect(page.getByText(text, { exact: true })).toBeVisible();
+  }
+});
